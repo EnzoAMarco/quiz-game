a9ed3287663a7bc26893414d6dfd52c7490ee00e
EnzoAMarco <enzom945@gmail.com>
2023-10-30 14:24:22 -0300

    paso 3 0

--- main.py
+++ main.py
@@ -4,26 +4,32 @@ from time import perf_counter
 
 preguntas = [
             'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;1',
-            # 'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;2',
+            'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;2',
             # 'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;3',
             # 'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;4',
              ]
-
+# toma una lista de preguntas como parametro y devuelve un dic con correctas = nro de preguntas correactas y tiempo = segundos totales que tomo responder todo
 def paso3(preguntasLista):
 
-  t1_start = perf_counter() 
+  data_puntuacion = {'correctas': 0, 'tiempo': 0}
+  contador_inicio = perf_counter() 
 
   for i in preguntasLista:
     registro_spliteado = i.split(';')
     print(registro_spliteado[0], *enumerate(registro_spliteado[1:-1],1), sep='\n')
+
     if input('Respuesta: ') == registro_spliteado[-1]:
       print('Respuesta correcta')
+      data_puntuacion['correctas'] += 1
     else:
       print('Respuesta incorrecta, la respuesta correcta era la nro', int(registro_spliteado[-1]), registro_spliteado[int(registro_spliteado[-1])])
       
-  t1_stop = perf_counter()
+  contador_fin = perf_counter()
+  data_puntuacion['tiempo'] = round(contador_fin-contador_inicio)
 
   print('----------------------------------------------------------------')
-  print('Tiempo tardado', round(t1_stop-t1_start), 'segundos')
+  print('Tiempo tardado', round(contador_fin-contador_inicio), 'segundos')
   
-paso3(preguntas)
+  return data_puntuacion
+
+print(paso3(preguntas))
