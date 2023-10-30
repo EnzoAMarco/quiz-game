f6c11ce65dda675c7821137862dc9df4506d37a4
EnzoAMarco <enzom945@gmail.com>
2023-10-30 01:35:37 -0300

    mitad paso 3

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # quiz-game
+--------------------------------------------------------------------------------------------------------------
+# 1_nick y dificultad = dic{'nombre': , 'dificultad':}
+# 2_selecciona un conjunto de preguntas en base a la dificultad = lista[preguntas]
+# 3_printea preguntas y por pregunta el usuario responde || dice correcto o si es falso pone la respuesta correcta
+# 5_se informa la puntuacion al usuario y opcion a volver a ejecutar
+# 6_el resultado se basa en el tiempo total, dificultad y preguntas correctas
+# 7_termina de ejctura el programa y se informan todos los participantes de la sesion ordenados segun el puntaje obtenido
+# 8_estos se archivaran en un archivo participantes.txt
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-# uwu
+from time import perf_counter
+
+# 3
+
+preguntas = [
+            'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;1',
+            # 'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;2',
+            # 'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;3',
+            # 'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;4',
+             ]
+
+def paso3(preguntasLista):
+
+  t1_start = perf_counter() 
+
+  for i in preguntasLista:
+    registro_spliteado = i.split(';')
+    print(registro_spliteado[0], *enumerate(registro_spliteado[1:-1],1), sep='\n')
+    if input('Respuesta: ') == registro_spliteado[-1]:
+      print('Respuesta correcta')
+    else:
+      print('Respuesta incorrecta, la respuesta correcta era la nro', int(registro_spliteado[-1]), registro_spliteado[int(registro_spliteado[-1])])
+      
+  t1_stop = perf_counter()
+
+  print('----------------------------------------------------------------')
+  print('Tiempo tardado', round(t1_stop-t1_start), 'segundos')
+  
+paso3(preguntas)
