e495049a3cb27b0662b62d9b5b2d97dffdb545de
braguilera <braaguileraa@gmail.com>
2023-10-31 00:14:33 -0300

    Puntaje 2 final + resursividad

--- main.py
+++ main.py
@@ -5,71 +5,110 @@ import random
 '''El contadora de dificil lo saque porque no hacen falta
 para sacar el dificil se le suma dos al contador de medio hasta el contador general
 los demas ya estan definidos con sus propios contadores
-contf+3,contf+contm
+
 '''
-try:
-  preg=open('preguntas.txt','rt')
 
-  nick='robert'
-  dificultad='m'
+def cargar_preguntas():
 
-  linea=preg.readline()
+  try:
+    preg=open('preguntas.txt','rt')
 
-  contf=-1
-  contm=-1
-  contgeneral=-3
-  flagf=0
-  flagm=-1
+    nick='robert'
+    dificultad='m'
 
-  while linea:
-      linea=linea.rstrip('\n')
-      contgeneral+=1
+    linea=preg.readline()
 
-      if linea=='dificil':
-        flagm=0
+    contf=-1
+    contm=-1
+    contgeneral=-3
+    flagf=0
+    flagm=-1
 
-      elif linea=='medio' or flagm==1:
-        flagf=0
-        contm+=1
-        flagm=1
-
-      elif linea=='facil' or flagf==1:
-        contf+=1
-        flagf=1
-
-      linea=preg.readline()
-
-  lista_preg=[]
-
-  for i in range(5):
-  
-    preg.seek(0)
-
-    if dificultad=='f':
-      num_preg=random.randint(1,contf)
-
-    elif dificultad=='m':
-      num_preg=random.randint(1,7)
-
-    elif dificultad=='d':
-      num_preg=random.randint(contf+contm+1,contgeneral)
-
-
-    for num_txt,linea in enumerate(preg):
-    
-      if int(num_txt)==num_preg:
-
+    while linea:
         linea=linea.rstrip('\n')
-        lista_preg.append(linea)
+        contgeneral+=1
 
-  print(lista_preg)
+        if linea=='dificil':
+          flagm=0
 
-except OSError as mensaje:
-    print('No se pudo grabar el archivo:', mensaje)
+        elif linea=='medio' or flagm==1:
+          flagf=0
+          contm+=1
+          flagm=1
 
-finally:
-    try:
-        preg.close()
-        
-    except NameError:
-        pass
+        elif linea=='facil' or flagf==1:
+          contf+=1
+          flagf=1
+
+        linea=preg.readline()
+
+    cont_preg=0
+
+    lista_repetidos=[]
+    lista_preg=[]
+
+    while cont_preg<5:
+      preg.seek(0)
+      cont_preg+=1
+
+      if dificultad=='f':
+        num_preg=random.randint(1,contf)
+
+      elif dificultad=='m':
+        num_preg=random.randint(contf+3,contf+contm)
+
+      elif dificultad=='d':
+        num_preg=random.randint(contf+contm+1,contgeneral)
+
+      while num_preg in lista_repetidos:
+
+        if dificultad=='f':
+          num_preg=random.randint(1,contf)
+
+        elif dificultad=='m':
+          num_preg=random.randint(contf+3,contf+contm)
+
+        elif dificultad=='d':
+          num_preg=random.randint(contf+contm+1,contgeneral)
+
+      lista_repetidos.append(num_preg)
+      for num_txt,linea in enumerate(preg):
+
+        if int(num_txt)==num_preg:
+          linea=linea.rstrip('\n')
+          lista_preg.append(linea)
+
+  except OSError as mensaje:
+      print('No se pudo grabar el archivo:', mensaje)
+
+  finally:
+      try:
+          preg.close()
+          
+      except NameError:
+          pass
+      
+
+def jugar(preg_por_difcultad,puntaje_actual):
+  
+  if preg_por_difcultad==0:
+
+    return puntaje_actual
+  
+  pregunta_actual=cargar_preguntas()
+
+  #es una lista, que devuelve una pregunta para hacer, hacer_pregunta(pregunta_actual)
+
+  respuesta=int(input('Ingrese su respuesta: '))
+
+  if verificar_respuesta():
+    print('CORRECTO!')
+    puntaje_actual+=1
+
+  else:
+    print(f'INCORRECTO, respuesta correcta ({})')
+
+  juego(preg_por_difcultad-1,puntaje_actual)
+
+
+
