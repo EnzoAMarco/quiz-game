9ec674df5538ae437da2865668f8108eb4aaf3a6
julian <julianbonavota@gmail.com>
2023-11-03 20:02:22 -0300

    paso 5 y 6

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ def nombre_dificultad():
   return datos_turno
 
 #print(nombre_dificultad()["nick"])
+def paso_5(dic1,dic2):
+  print(f"feliciades {dic2["nick"]}, jugando en la dificultad {dic2["dificultad"]}, has obtenido {dic1["correctas"]}pts en {dic1["tiempo"]}''")
+#diccionario= [{"nick":dic2["nick"], "dificultad":dic2["dificultad"],"correctas":dic1["correctas"], "tiempo":dic1["tiempo"]},]
+  return dic1|dic2
+
+di1 = {'correctas': 3, 'tiempo': 10}
+di2= {"nick":"Artenam","dificultad":"medio"}
+
