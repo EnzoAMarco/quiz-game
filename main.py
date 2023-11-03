5af8b204b42d2c7beca03a0bbf9cde175bb7852e
EnzoAMarco <enzom945@gmail.com>
2023-11-03 20:17:32 -0300

    e+j

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from time import perf_counter
 
-# 3
+def nombre_dificultad():
+  nick1=input("ingrese su nombre: ")
+  dificultad1=input("ingrese la dificultad (f para facil, n normal, d dificl): ").lower()
+
+  while dificultad1 != "f" and dificultad1!= "n" and  dificultad1!= "d":
+    dificultad1=input("ERROR SELECCIONE UNA DIFICULTAD VALIDA (f para facil, n normal, d dificl): ").lower()
+
+  datos_turno={"nick":nick1,"dificultad":dificultad1}
+  return datos_turno
+
+#print(nombre_dificultad()["nick"])
+def paso_5(dic1,dic2):
+  print(f"feliciades {dic2['nick']}, jugando en la dificultad {dic2['dificultad']}, has obtenido {dic1['correctas']}pts en {dic1['tiempo']}")
+  return dic1|dic2
+
+di1 = {'correctas': 3, 'tiempo': 10}
+di2= {"nick":"Artenam","dificultad":"medio"}
 
 preguntas = [
             'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;1',
@@ -38,7 +54,7 @@ def paso7(datos):
 
   print('\n'.join([' '.join(map(str, fila)) for fila in sorted(datos , key=lambda x: x[2], reverse=True)]))
 
-paso7([['owo','m',3],['uwu','h',5],['awa','f',1]])
+# paso7([['owo','m',3],['uwu','h',5],['awa','f',1]])
 
 def paso8(lista, direA1):
     
