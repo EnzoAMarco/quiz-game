8eb938f271a841af5a8a9d3f3204c069713073b1
julian <julianbonavota@gmail.com>
2023-11-03 22:36:40 -0300

    no repetir preguntas

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 def nombre_dificultad():
   nick1=input("ingrese su nombre: ")
   dificultad1=input("ingrese la dificultad (f para facil, n normal, d dificl): ").lower()
@@ -14,6 +15,34 @@ def paso_5(dic1,dic2):
 #diccionario= [{"nick":dic2["nick"], "dificultad":dic2["dificultad"],"correctas":dic1["correctas"], "tiempo":dic1["tiempo"]},]
   return dic1|dic2
 
-di1 = {'correctas': 3, 'tiempo': 10}
-di2= {"nick":"Artenam","dificultad":"medio"}
+def no_repetir():
+  preguntar=set()
+  try:
+    a1 = open(r"C:\Users\Usuario\Desktop\uade\Algoritmia\quiz-game\PREGUNTAS.txt",'rt')
+    while len(preguntar)<5:
+      question = a1.readline()
+      preguntar.add(question)
 
+  except FileNotFoundError as md:
+    print(md)
+
+  except OSError as md:
+    print('error so', md)
+
+  finally:
+    try:
+      a1.close()
+
+    except NameError as md:
+      print(md)
+      pass
+
+    
+  return preguntar
+
+
+  
+
+#preguntas=(1,2,3,3,2,2,2,4,4,5)
+x=no_repetir()
+print(x)
