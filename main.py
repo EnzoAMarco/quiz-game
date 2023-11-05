debcfcc2b2253fda06152569147def53e7291225
EnzoAMarco <enzom945@gmail.com>
2023-11-05 15:10:13 -0300

    func finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from time import perf_counter
 
-def nombre_dificultad():
+def paso1():
   nick1=input("ingrese su nombre: ")
   dificultad1=input("ingrese la dificultad (f para facil, n normal, d dificl): ").lower()
 
@@ -10,20 +10,88 @@ def nombre_dificultad():
   datos_turno={"nick":nick1,"dificultad":dificultad1}
   return datos_turno
 
-#print(nombre_dificultad()["nick"])
-def paso_5(dic1,dic2):
-  print(f"feliciades {dic2['nick']}, jugando en la dificultad {dic2['dificultad']}, has obtenido {dic1['correctas']}pts en {dic1['tiempo']}")
-  return dic1|dic2
+def paso2(dificultad):
 
-di1 = {'correctas': 3, 'tiempo': 10}
-di2= {"nick":"Artenam","dificultad":"medio"}
+  try:
+    preg=open('preguntas.txt','rt')
 
-preguntas = [
-            'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;1',
-            'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;2',
-            # 'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;3',
-            # 'Quien "sabia que no sabia nada"?;Aristoteles;Ortega y Gasset;Socrates;Platon;4',
-             ]
+
+    linea=preg.readline()
+
+    contf=-1
+    contm=-1
+    contgeneral=-3
+    flagf=0
+    flagm=-1
+
+    while linea:
+        linea=linea.rstrip('\n')
+        contgeneral+=1
+
+        if linea=='dificil':
+          flagm=0
+
+        elif linea=='medio' or flagm==1:
+          flagf=0
+          contm+=1
+          flagm=1
+
+        elif linea=='facil' or flagf==1:
+          contf+=1
+          flagf=1
+
+        linea=preg.readline()
+
+    cont_preg=0
+
+    lista_repetidos=[]
+    lista_preg=[]
+
+    while cont_preg<5:
+      preg.seek(0)
+      cont_preg+=1
+
+      if dificultad=='f':
+        num_preg=random.randint(1,contf)
+
+      elif dificultad=='m':
+        num_preg=random.randint(contf+3,contf+contm)
+
+      elif dificultad=='d':
+        num_preg=random.randint(contf+contm+1,contgeneral)
+
+      while num_preg in lista_repetidos:
+
+        if dificultad=='f':
+          num_preg=random.randint(1,contf)
+
+        elif dificultad=='m':
+          num_preg=random.randint(contf+3,contf+contm)
+
+        elif dificultad=='d':
+          num_preg=random.randint(contf+contm+1,contgeneral)
+
+      lista_repetidos.append(num_preg)
+      for num_txt,linea in enumerate(preg):
+
+        if int(num_txt)==num_preg:
+          linea=linea.rstrip('\n')
+          lista_preg.append(linea)
+
+    return lista_preg
+
+  except OSError as mensaje:
+      print('No se pudo grabar el archivo:', mensaje)
+
+  finally:
+      try:
+          preg.close()
+          
+      except NameError:
+          pass
+
+preguntas = paso2()
+
 # toma una lista de preguntas como parametro y devuelve un dic con correctas = nro de preguntas correactas y tiempo = segundos totales que tomo responder todo
 def paso3(preguntasLista):
 
@@ -50,13 +118,20 @@ def paso3(preguntasLista):
 
 # print(paso3(preguntas))
 
-def paso7(datos):
+def paso_4(dic1,dic2):
+  print(f"feliciades {dic2['nick']}, jugando en la dificultad {dic2['dificultad']}, has obtenido {dic1['correctas']}pts en {dic1['tiempo']}")
+  return dic1|dic2
+
+di1 = {'correctas': 3, 'tiempo': 10}
+di2= {"nick":"Artenam","dificultad":"medio"}
+
+def paso5(datos):
 
   print('\n'.join([' '.join(map(str, fila)) for fila in sorted(datos , key=lambda x: x[2], reverse=True)]))
 
 # paso7([['owo','m',3],['uwu','h',5],['awa','f',1]])
 
-def paso8(lista, direA1):
+def paso6(lista, direA1):
     
   try:
     a1 = open(direA1,'at')
